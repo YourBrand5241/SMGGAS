5c3a473c2288c0678fcf06f9729bb2c9d1d0a4d3
YourBrand5241 <yourbrand5241@gmail.com>
2026-09-25 14:05:41 +0100

    Add files via upload

--- quote.js
+++ quote.js
@@ -4,7 +4,7 @@ const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_8-VfhsJiclZMwjjkZ-k18A_gLYKbaGR
 const BUSINESS_ID = "smg-gas";
 const BUSINESS_NAME = "SMG Gas";
 
-const EMAILJS_SERVICE_ID = "service_zzjha2e";
+const EMAILJS_SERVICE_ID = "service_m08877i";
 const EMAILJS_TEMPLATE_ID = "template_khedkjr";
 const EMAILJS_PUBLIC_KEY = "fs6q7ZsiYGhRUtas5";
 const OWNER_EMAIL = "stephen@smggas.com";
